45a77efb2ca2e8d1836df5c44b0f8cc167bba925
Andrii Ovdiienko <Andrii.Ovdiienko@medhost.com>
2016-04-12 18:38:54 +0300

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,17 @@ var uglify = require('gulp-uglify');
 var minifyCss =  require('gulp-minify-css');
 var less =  require('gulp-less');
 var path = require('path');
+var changed = require('gulp-changed');
+var minifyHTML = require('gulp-minify-html');
+var connect = require('gulp-connect');
+var order = require('gulp-order');
+
+gulp.task('connect', function () {
+    connect.server({
+        root: 'src/',
+        port: 8888
+    });
+});
 
 gulp.task('less', function() {
   gulp.src(['src/less/**/*.less'])
@@ -21,18 +32,31 @@ gulp.task('css', function() {
 
 gulp.task('vendors', function() {
   gulp.src(['vendor/**/*.js'])
+    .pipe(order([
+          'vendor/angular/angular.min.js',
+          'vendor/angular-bootstrap/ui-bootstrap.min.js',
+          'vendor/**/*.js'
+      ]))
     .pipe(concat('bin/vendors.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('.'))
+    .pipe(gulp.dest('bin'))
 });
 
 gulp.task('js', function() {
   gulp.src(['src/**/*Router.js', 'src/**/*.js'])
-    .pipe(concat('application.js'))
+    .pipe(concat('bin/application.js'))
     .pipe(uglify())
     .pipe(gulp.dest('.'))
 });
 
+gulp.task('html', function() {
+  gulp.src(['src/*.html'])
+      .pipe(changed('src'))
+      .pipe(minifyHTML())
+      .pipe(gulp.dest('bin'))
+});
+
+gulp.task('default', ['less', 'css', 'vendors', 'js', 'html'], function() {});
 
 // example for build
 // .pipe(less({
